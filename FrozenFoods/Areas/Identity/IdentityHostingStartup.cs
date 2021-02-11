eb3884b917ed0ddbd295e99b00ce1ed8168e46a6
AK054 <khoslaabhi3@gmail.com>
2021-02-12 09:42:47 +1300

    Add project files.

---
+++ FrozenFoods/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FrozenFoods.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FrozenFoods.Areas.Identity.IdentityHostingStartup))]
+namespace FrozenFoods.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ FrozenFoods/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FrozenFoods.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrozenFoods.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<FoodCategory> FoodCategories { get; set; }
+        public DbSet<FoodInfo> FoodInfos { get; set; }
+        public DbSet<FoodOrder> FoodOrders { get; set; }
+    }
+}
